4a6291bbf0dc6a3314dc29367a969996f299518e
xholiday81x@gmail.com <xholiday81x@gmail.com>
2022-08-01 19:25:25 -0500

    first

---
+++ fxmanifest.lua
@@ -0,0 +1,9 @@
+fx_version 'cerulean'
+games { 'gta5' }
+
+author 'Holiday'
+description 'ARP Paleto Central Maps. Liquor Store, Blue Line, City Hall, Pharmacy, Broken Club House, Paleto Medical, Dreamview Exterior'
+version 'v1.00.1'
+
+this_is_a_map 'yes'
+
